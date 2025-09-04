ea738ae9bb55922ef9e5a478c78bc51a67f9e6ec
kimhyeji <gpwl5589@gmail.com>
2025-09-04 18:36:02 +0900

    13

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ setInterval(function() {
 	$('.slider-box').attr('data-index', ++sliderBoxIndex % 3);
 	console.log( ++sliderBoxIndex % 3)
 }, 3000);
+
+function tabBoxInit() {
+    $('.tab-head > ul > li').click(function() {
+        let $this = $(this);
+        let thisIndex = $this.index();
+
+        let $tabBox = $this.closest('.tab-box');
+        let $tabBody = $tabBox.find(' > .tab-body');
+
+        $this.siblings('.active').removeClass('active');
+        $this.addClass('active');
+
+        $tabBody.find(' > ul > li.active').removeClass('active');
+        $tabBody.find(' > ul > li').eq(thisIndex).addClass('active');
+    })
+}
+
+tabBoxInit();
